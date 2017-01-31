c1c34546c8ce383a208381032b550cbd2eaf3fca
Alexander Balashov <Alexander.Balashov@karlstorz.com>
2017-01-31 14:35:29 +0100

    Added open cv lib

---
+++ gstreamer/dvs/analyzersrv/src/main/java/aib/dvs/av/RTPCaptureTask.java
@@ -0,0 +1,41 @@
+package aib.dvs.av;
+
+import org.apache.log4j.Logger;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RTPCaptureTask implements Runnable {
+	static Logger logger = Logger.getLogger(RTPCaptureTask.class);
+	VideoCapture capture;
+	
+	@Override
+	public void run() {
+		capture = new VideoCapture();
+		capture.open("udp://@224.1.1.1:5200");
+		if(!capture.isOpened()) {
+			logger.error("Can not open rtp stream!");
+			return;
+		}
+
+		Mat image=new Mat();
+		
+		for(;;) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			if(capture.read(image)) {
+				
+				Imgcodecs.imwrite("d:\\Videos\\" + System.currentTimeMillis() + ".jpg", image);
+			}
+		}
+		
+//		capture.release();
+	}
+
+}
